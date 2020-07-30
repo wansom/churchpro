ef8b4b1d626362e480fe1f27bb9469a8736cf4e0
joyrisen <68549204+joyrisen@users.noreply.github.com>
2020-07-30 23:22:43 +0300

    final changes before shipping first apk

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -18,29 +18,7 @@ class _HomePageState extends State<HomePage> {
         body: SafeArea(
             child: ListView(
           children: <Widget>[
-            SizedBox(height: 20.0),
-            Padding(
-              padding: EdgeInsets.only(left: 15.0, right: 15.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Container(
-                    height: 40.0,
-                    width: 40.0,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(7.0),
-                        image: DecorationImage(
-                            image: AssetImage('assets/tuxedo.png'),
-                            fit: BoxFit.cover)),
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.notifications),
-                    color: Colors.black,
-                  ),
-                ],
-              ),
-            ),
+            SizedBox(height: 50.0),
             SizedBox(height: 10.0),
             Padding(
                 padding: EdgeInsets.only(left: 15.0, right: 15.0),
