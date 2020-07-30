dfa329e035581280d283e92c220cf4fd182648ce
joyrisen <68549204+joyrisen@users.noreply.github.com>
2020-07-30 11:20:50 +0300

    profile screen

--- lib/screens/home/profile.dart
+++ lib/screens/home/profile.dart
@@ -1,4 +1,7 @@
+import 'package:churchpro/services/auth/auth_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ProfilePage extends StatefulWidget {
   @override
@@ -6,12 +9,115 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  AuthService _auth = AuthService();
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<FirebaseUser>(context);
     return Scaffold(
-      appBar: AppBar(
-        title: Text('User Profile'),
-      ),
-    );
+        body: new Stack(
+      children: <Widget>[
+        ClipPath(
+          child: Container(color: Colors.green.withOpacity(0.8)),
+          clipper: GetClipper(),
+        ),
+        Positioned(
+            width: 350.0,
+            top: MediaQuery.of(context).size.height / 5,
+            child: Column(
+              children: <Widget>[
+                Container(
+                    width: 150.0,
+                    height: 150.0,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        image: DecorationImage(
+                            image: NetworkImage(
+                                'https://pixel.nymag.com/imgs/daily/vulture/2017/06/14/14-tom-cruise.w700.h700.jpg'),
+                            fit: BoxFit.cover),
+                        borderRadius: BorderRadius.all(Radius.circular(75.0)),
+                        boxShadow: [
+                          BoxShadow(blurRadius: 7.0, color: Colors.black)
+                        ])),
+                SizedBox(height: 50.0),
+                Text(
+                  'ScanPay User',
+                  style: TextStyle(
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Montserrat'),
+                ),
+                SizedBox(height: 15.0),
+                Text(
+                  user.email,
+                  style: TextStyle(
+                      fontSize: 17.0,
+                      fontStyle: FontStyle.italic,
+                      fontFamily: 'Montserrat'),
+                ),
+                SizedBox(height: 25.0),
+                Container(
+                    height: 30.0,
+                    width: 95.0,
+                    child: Material(
+                      borderRadius: BorderRadius.circular(20.0),
+                      shadowColor: Colors.greenAccent,
+                      color: Colors.green,
+                      elevation: 7.0,
+                      child: GestureDetector(
+                        onTap: () {},
+                        child: Center(
+                          child: Text(
+                            'Edit Profile',
+                            style: TextStyle(
+                                color: Colors.white, fontFamily: 'Montserrat'),
+                          ),
+                        ),
+                      ),
+                    )),
+                SizedBox(height: 25.0),
+                Container(
+                    height: 30.0,
+                    width: 95.0,
+                    child: Material(
+                      borderRadius: BorderRadius.circular(20.0),
+                      shadowColor: Colors.redAccent,
+                      color: Colors.red,
+                      elevation: 7.0,
+                      child: GestureDetector(
+                        onTap: () {
+                          _auth.signOut();
+                        },
+                        child: Center(
+                          child: Text(
+                            'Log out',
+                            style: TextStyle(
+                                color: Colors.white, fontFamily: 'Montserrat'),
+                          ),
+                        ),
+                      ),
+                    ))
+              ],
+            ))
+      ],
+    ));
+  }
+}
+
+class GetClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = new Path();
+
+    path.lineTo(0.0, size.height / 1.9);
+    path.lineTo(size.width + 125, 0.0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    // ignore: todo
+    // TODO: implement shouldReclip
+    return true;
   }
 }
--- lib/screens/home/scan.dart
+++ lib/screens/home/scan.dart
@@ -11,15 +11,22 @@ class ScanProduct extends StatefulWidget {
 }
 
 class _ScanProductState extends State<ScanProduct> {
-  var resultName = 'no results';
-  var resultCode = '';
-  var resultPrice = '';
-  var resultInfo = '';
-  var resultImage = '';
+  var resultName = 'Please refresh & scan again';
+  var resultCode = '03600291452';
+  var resultPrice = '0';
+  var resultInfo = 'No products match the code scanned';
+  var resultImage =
+      'https://www.google.com/url?sa=i&url=https%3A%2F%2Ftowardsdatascience.com%2Fa-standalone-application-to-scan-barcode-using-webcam-e09e44d9dbb&psig=AOvVaw2GmzCBDPLUzwTyQGqAqus-&ust=1596176210252000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIDg2JCq9OoCFQAAAAAdAAAAABAt';
   var netPrice = 0.0;
   var quantity = 1;
   int quant = 1;
   int sum = 0;
+  bool notScanned = true;
+  void toggleView() {
+    setState(() {
+      notScanned = !notScanned;
+    });
+  }
 
   ScanResult barcode;
   CartService cartServiceInstance = CartService();
@@ -141,16 +148,19 @@ class _ScanProductState extends State<ScanProduct> {
                           ],
                         ),
                       ),
-                      Container(
-                        height: 50.0,
-                        width: 50.0,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15.0),
-                          color: Colors.white,
-                        ),
-                        child: Center(
-                          child: Icon(Icons.restore,
-                              color: Color(0xFFFE7D6A), size: 25.0),
+                      InkWell(
+                        onTap: toggleView,
+                        child: Container(
+                          height: 50.0,
+                          width: 50.0,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15.0),
+                            color: Colors.white,
+                          ),
+                          child: Center(
+                            child: Icon(Icons.restore,
+                                color: Color(0xFFFE7D6A), size: 25.0),
+                          ),
                         ),
                       ),
                     ])
@@ -231,21 +241,6 @@ class _ScanProductState extends State<ScanProduct> {
                     ],
                   ))
             ]),
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Text(
-                'FEATURED',
-                style: GoogleFonts.notoSans(
-                    fontSize: 16.0, fontWeight: FontWeight.w700),
-              ),
-            ),
-            // Container(
-            //     height: 225.0,
-            //     width: MediaQuery.of(context).size.width,
-            //     child: ListView(
-            //       scrollDirection: Axis.horizontal,
-            //       children: <Widget>[_buildListItem('1'), _buildListItem('2')],
-            //     ))
           ],
         );
       } else {
@@ -268,7 +263,7 @@ class _ScanProductState extends State<ScanProduct> {
     return Scaffold(
       body: SafeArea(
         child: Container(
-          child: myText(),
+          child: notScanned ? myText() : Text('helo'),
         ),
       ),
     );
@@ -298,10 +293,28 @@ class _ScanProductState extends State<ScanProduct> {
           });
         });
       } else {
-        print('result not found');
+        await Firestore.instance
+            .collection('products')
+            .where('productcode', isEqualTo: '03600291452')
+            .getDocuments()
+            .then((value) {
+          value.documents.forEach((element) {
+            setState(() {
+              resultName = element.data['productname'] ?? 'no results found';
+              resultCode = element.data['productcode'] ?? 'no results found';
+              resultPrice = element.data['productprice'] ?? 'no results found';
+              resultInfo = element.data['productinfo'] ?? 'no results found';
+              resultImage = element.data['imageurl'] ?? '';
+            });
+          });
+        });
       }
     } on PlatformException catch (e) {
       print(e);
+    } on FormatException {
+      setState(() {
+        barcode.rawContent = '03600291452';
+      });
     }
   }
 
